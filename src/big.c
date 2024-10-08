15f2773b7808403ee131ea02c6e328a91d04914b
Gerben Aaltink <gerbenaaltink@gmail.com>
2024-10-08 22:43:56 +0200

    Formatted

--- src/big.c
+++ src/big.c
@@ -1,5 +1,5 @@
-#include <rlib.h>
 #include "big.h"
+#include <rlib.h>
 #define print rprintb
 int printf_dummy(const char *format, ...) {
     (void)format;
@@ -12,46 +12,44 @@ int printf_dummy(const char *format, ...) {
 
 #include "parser.h"
 
-
 unsigned int parse_only(char *script) {
-    lexer_t * lexer = lexer_new();
-     lexer = lexer_parse(lexer, script);
+    lexer_t *lexer = lexer_new();
+    lexer = lexer_parse(lexer, script);
     parse(lexer);
     unsigned int result = lexer->lines;
     lexer_delete(lexer);
     return result;
 }
-void lex_and_parse(char * script){
-     parse_string(script);
-}
+void lex_and_parse(char *script) { parse_string(script); }
 
 int main() {
     char *script = big_dob;
+    lexer_t *lexer = lexer_new();
+    lexer = lexer_parse(lexer, script);
+
+    print("Only lexing:\n");
     nsecs_t time_start = nsecs();
-    lexer_t * lexer = lexer_new();
-     lexer = lexer_parse(lexer, script);
     parse(lexer);
     unsigned int lines = lexer->lines;
     unsigned int token_count = lexer->count;
     lexer_delete(lexer);
     nsecs_t time_end = nsecs();
     nsecs_t duration = time_end - time_start;
-    print("Full parsing (lex+parse):\n");
     print("Time: %s\n", format_time(duration));
-     print("Lines per second: %f\n",
-           ((float)lines) / (duration / 1000000000.0f));
-    
-      time_start = nsecs();
+    print("Lines per second: %f\n",
+          ((float)lines) / (duration / 1000000000.0f));
+    print("Full parsing (lexing+parsing):\n");
+    time_start = nsecs();
     parse_string(script);
-     time_end = nsecs();
-     duration = time_end - time_start;
-    print("Full parsing (lex+parse):\n");
+    time_end = nsecs();
+    duration = time_end - time_start;
+
     print("Time: %s\n", format_time(duration));
-     print("Lines per second: %f\n",
-           ((float)lines) / (duration / 1000000000.0f));
-    
+    print("Lines per second: %f\n",
+          ((float)lines) / (duration / 1000000000.0f));
+
     print("Lines: %d\n", lines);
-    print("Token count: %zu\n", token_count); 
+    print("Token count: %zu\n", token_count);
     print("%s\n", rmalloc_stats());
     return 0;
 }
